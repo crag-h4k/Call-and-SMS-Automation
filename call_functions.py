f4ff58b4164cfeb4f980d8ca68b5abb5171ffe67
Dane Morgan <danemorgan91@gmail.com>
2018-02-24 16:46:53 -0700

    library call works.... noob mistake

--- call_functions.py
+++ call_functions.py
@@ -6,26 +6,34 @@ def addNumber():
     while number != 'q':
         number = input('Number:\nEnter \'q\' to quit.')
     sp.call(['echo ',number,' >> spammers.txt'])
-    return
+    print("Success")
 
 #load phone numbers
 def loadPhoneNums():
     import pygooglevoice
     with open(spammers.txt, 'r') as file:
-        phoneNumbers = file.read().replace('\n','')
+        recipientNums = file.read().replace('\n','')
     file.close()
+    print("Success")
     return recipientNums
 
 #text to speech here and saves the text as an MP3
-def speechToText():
+def textToSpeech():
     from gtts import gTTS
     import subprocess as sp
 
-    #text = input("What do you want to say?")
-    tts = gTTS(text='This is a text', lang='en')
+    inputText = input("What do you want to say?\n")
+    tts = gTTS(text=inputText, lang='en')
     tts.save('audio.mp3')
-    sp.call(['afplay audio.mp3'])    
-    
+    #sp.call('afplay audio.mp3', shell=True)    
+    listen = input('Do you want to hear the audio file? y/n\n')
+    if listen == 'y':
+        sp.call('afplay audio.mp3' ,shell = True)
+    else:
+        #print("Fail")
+        return
+    print("Success")
+ 
 def sendText(recipientNums):
     from googlevoice import Voice
     from googlevoice.util import input
--- menu.py
+++ menu.py
@@ -1,3 +1,38 @@
-import call_functions
 
-print('Success!')
+#print('Success!')
+def printMenu():
+    import call_functions as cf
+   
+#menu items here 
+    print('Main Menu\n------------------------------')
+    print('Options:')
+    print('sms\t\tSend SMS')
+#    print('call\t\tMake a Call')
+    print('audio\t\tChange call audio')
+    print('add\t\tAdd Phone Number to List')
+    print('load\t\tLoad Phone Numbers')
+    print('help\t\tPrint Menu')
+    print('q\t\tQuit')
+
+    option = input('~')
+ 
+    if option == 'sms':
+        cf.sendText()
+#   elif option == 'call':
+#       cf.makeCall()
+    elif option == 'audio':
+        cf.textToSpeech()
+    elif option == 'add':
+        cf.addNumber()
+    elif option == 'load':
+        cf.loadPhoneNums()
+    elif option == 'help':
+        printMenu()
+    elif option == 'q':
+        exit()
+    else:
+        print("enter new input")
+    option = '~'
+    printMenu()
+
+printMenu()
